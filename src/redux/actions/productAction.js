4640420bb07601ffb7c99a6e9a047ab3b8079e5c
Tarun Verma <verma.tarun200109@gmail.com>
2022-01-14 18:49:42 +0530

    final commit

--- src/redux/actions/productAction.js
+++
@@ -1,7 +0,0 @@
-import { ActionTypes } from '../constants/actionTypes';
-export const setProducts = (products) => {
-  return {
-    type: ActionTypes.SET_PRODUCTS,
-    payload: products,
-  };
-};
